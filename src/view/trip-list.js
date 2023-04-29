aa027a97ac9d54cfb34deeb6432a8139f4aa6676
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2023-04-29 18:06:41 +0300

    View and presenter

---
+++ src/view/trip-list.js
@@ -0,0 +1,23 @@
+import { createElement } from "../render";
+
+function createTripList () {
+    return `<ul class="trip-events__list"></ul>`
+}
+
+export default class TripList {
+    getTemplate() {
+        return createTripList()
+    }
+
+    getElement() {
+        if(!this.element) {
+            this.element = createElement(this.getTemplate())
+        }
+        
+        return this.element
+    }
+
+    removeElement () {
+        this.element = null
+    }
+}
